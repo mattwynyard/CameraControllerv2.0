18704c96c11f24272ec97ccdfcf970edb3cebded
mjwynyard@gmail.com <mjwynyard@gmail.com>
2022-10-05 07:26:01 +1300

    tidy up - office sync

--- src/main/java/TCPConnection/TCPServer.java
+++ src/main/java/TCPConnection/TCPServer.java
@@ -33,7 +33,6 @@ public class TCPServer {
             CameraApp.logError(err.toString());
             System.out.println("Error: " + err);
         }
-
     }
 
     //called from SPPClient before thread start
@@ -43,14 +42,14 @@ public class TCPServer {
 
     public void start() {
         try {
-                Socket client = server.accept();
-                clients.add(client);
-                System.out.println("Access client connected on socket: " + client.toString());
-                //System.out.println("Clients connected: " + clients.size());
-                accessWriter = new PrintWriter(client.getOutputStream()); //access
-                mReadAccessThread = new Thread(readFromAccess);
-                mReadAccessThread.setPriority(Thread.MAX_PRIORITY);
-                mReadAccessThread.start();
+            Socket client = server.accept();
+            clients.add(client);
+            System.out.println("Access client connected on socket: " + client.toString());
+            accessWriter = new PrintWriter(client.getOutputStream());
+            Runnable phoneThread = new AccessThread(String.valueOf(this.hasPhone));//access
+            mReadAccessThread = new Thread(phoneThread);
+            mReadAccessThread.setPriority(Thread.MAX_PRIORITY);
+            mReadAccessThread.start();
         } catch (IOException err) {
             CameraApp.logError(err.toString());
             System.out.println("Error: " + err);
@@ -66,8 +65,8 @@ public class TCPServer {
             System.out.println("Client connected: " + client.toString());
             System.out.println("Clients connected: " + clients.size());
             mapWriter = new PrintWriter(client.getOutputStream()); //map
-            mReadMapThread = new Thread(readFromMap);
-            if (!phoneConnected) mReadMapThread.setPriority(Thread.MAX_PRIORITY);
+            mReadMapThread = new Thread(MapThread);
+            if (!hasPhone) mReadMapThread.setPriority(Thread.MAX_PRIORITY);
             mReadMapThread.start();
         } catch (IOException err) {
             CameraApp.logError(err.toString());
@@ -124,51 +123,8 @@ public class TCPServer {
             e.printStackTrace();
         }
     }
-    /**
-     * To run in the background,  reads incoming data
-     * from the client - access
-     */
-    private final Runnable readFromAccess = new Runnable() {
-        @Override
-        public void run() {
-            System.out.println("Access Thread listening");
-            int length;
-            byte[] buffer = new byte[1024];
-            try {
-                accessReader = clients.get(0).getInputStream();
-                phoneConnected = true;
-                while ((length = accessReader.read(buffer)) != -1) {
-                    String line = new String(buffer, 0, length);
-                    if (line.equals("Start")) {
-                        //if (this.hasPhone){
-                            sendDataAndroid(line);
-                        //}
-                    } else if (line.equals("Stop")) {
-                        //if (this.phone) {
-                            sendDataAndroid(line);
-                        //}
-                    } else if (line.contains("Time")){
-                        //if (this.phone) {
-                            sendDataAndroid(line);
-                        //}
-                    } else {
-                        System.out.println(line);
-                    }
-                }
-                CameraApp.logError("access socket shutdown");
-                accessReader.close();
-            }
-            catch (IOException err) {
-                CameraApp.logError(err.toString());
-                err.printStackTrace();
-                phoneConnected = false;
-                System.out.println("Socket Shutdown");
-                System.exit(0);
-            }
-        }
-    };
 
-    private final Runnable readFromMap = new Runnable() {
+    private final Runnable MapThread = new Runnable() {
         @Override
         public void run() {
             System.out.println("Map Thread listening");
@@ -198,5 +154,84 @@ public class TCPServer {
             }
         }
     };
+
+    private class AccessThread implements Runnable {
+        boolean phone;
+        public AccessThread(Object o) {
+            if (o.equals("true")) {
+                this.phone = true;
+            } else if (o.equals("false")) {
+                this.phone = false;
+            }
+        }
+        public void run() {
+            System.out.println("Access Thread listening");
+            int length;
+            byte[] buffer = new byte[1024];
+            try {
+                accessReader = clients.get(0).getInputStream();
+                phoneConnected = true;
+                while ((length = accessReader.read(buffer)) != -1) {
+                    String line = new String(buffer, 0, length);
+                    if (line.equals("Start")) {
+                        if (this.phone){
+                        sendDataAndroid(line);
+                        }
+                    } else if (line.equals("Stop")) {
+                        if (this.phone) {
+                        sendDataAndroid(line);
+                        }
+                    } else if (line.contains("Time")){
+                        if (this.phone) {
+                        sendDataAndroid(line);
+                        }
+                    } else {
+                        System.out.println(line);
+                    }
+                }
+                CameraApp.logError("access socket shutdown");
+                accessReader.close();
+            }
+            catch (IOException err) {
+                CameraApp.logError(err.toString());
+                err.printStackTrace();
+                phoneConnected = false;
+                System.out.println("Socket Shutdown");
+                System.exit(0);
+            }
+        }
+    }
+
+//    private class MapThread implements Runnable {
+//        public MapThread() {
+//        }
+//        public void run() {
+//            System.out.println("Map Thread listening");
+//            int length;
+//            byte[] buffer = new byte[4 * 1024];
+//            try {
+//                mapReader = clients.get(1).getInputStream();
+//                mapConnected = true;
+//                while ((length = mapReader.read(buffer)) != -1) {
+//                    String line = new String(buffer, 0, length);
+//                    sendDataDB((line));
+//                }
+//                mapReader.close();
+//            }
+//            catch (Exception err) {
+//                err.printStackTrace();
+//                CameraApp.logError(err.toString());
+//                mapConnected = false;
+//                System.out.println("Map Socket Shutdown");
+//                clients.remove(1);
+//                try {
+//                    Thread.sleep(5000);
+//                } catch (Exception e) {
+//
+//                }
+//                startMap();
+//            }
+//        }
+//    }
 }
 
